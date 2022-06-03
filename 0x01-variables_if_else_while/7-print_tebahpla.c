f7e33ed1e026d7d76d6170e31fb65f38043a7c77
Olatundji <104756391+Olatundji@users.noreply.github.com>
2022-06-03 16:32:51 +0100

    Update 7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,33 +1,17 @@
-nclude <stdio.h>
-
-
+#include <stdio.h>
 
 /**
- *
- *  * main - prints alphabets in lowercase and uppercase,
- *
- *   * followed by a new line
- *
- *    * Return: Always 0 (Success)
- *
- *     */
-
+ * main - prints alphabets in lowercase and uppercase,
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
 int main(void)
-
 {
-
-	int i;
-
-	for (i = 'z'; i >= 'a'; i--)
-
-	{
-
-		putchar(i);
-
-	}
-
-	putchar('\n');
-
-	return (0);
-
+int i;
+for (i = 'z'; i >= 'a'; i--)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
 }
