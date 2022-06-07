b6e28daa59539fd5725eebe135de513c94de92f2
Olatundji <oikpaye@gmail.com>
2022-06-07 05:22:57 -0700

    Programming in c to day

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,35 @@
+nclude "main.h"
+
+
+
+/**
+ *
+ *  * print_alphabet - print alphabet
+ *
+ *   *
+ *
+ *    * Return: void
+ *
+ *     */
+
+void print_alphabet(void)
+
+{
+
+		char caract = 'a';
+
+
+
+			while (caract <= 'z')
+
+					{
+
+								_putchar(caract);
+
+										caract++;
+
+											}
+
+				_putchar('\n');
+
+}
