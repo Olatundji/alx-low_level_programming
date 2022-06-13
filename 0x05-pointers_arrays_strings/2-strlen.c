87ba73c688d8bd2f16e37a6d3a78e27c35f1d31a
Olatundji <104756391+Olatundji@users.noreply.github.com>
2022-06-13 14:52:46 +0100

    Update 2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,35 +1,18 @@
-nclude "main.h"
-
+#include "main.h"
 /**
- *
- *  * _strlen - returns the length of a string
- *
- *   * @word: a pointer to int that will be updated
- *
- *    * Return: 0
- *
- *     */
-
+ * _strlen - returns the length of a string
+ * @word: a pointer to int that will be updated
+ * Return: 0
+ */
 int _strlen(char *word)
-
 {
+int d;
 
-	int d;
+d = 0;
 
-
-
-	d = 0;
-
-
-
-	while (word[d] != '\0')
-
-	{
-
-		d++;
-
-	}
-
-	return (d);
-
+while (word[d] != '\0')
+{
+d++;
+}
+return (d);
 }
