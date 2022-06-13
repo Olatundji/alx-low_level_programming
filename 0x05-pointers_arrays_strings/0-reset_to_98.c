db6b24e5152fa5581e4f32d57ede40d119d57684
Olatundji <104756391+Olatundji@users.noreply.github.com>
2022-06-13 14:48:15 +0100

    Update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,21 +1,10 @@
 #include "main.h"
-
-
-
 /**
- *reset_to_98 - updates a integer value to 98
- *@n: A pointer to an int that will be updated
- *Return: void that means is correct
+ * reset_to_98 - update and interger value to 98
+ * @n: A pointer to an int that will be updated
+ * Return: 0
  */
-
-
-
 void reset_to_98(int *n)
-
-
-
 {
-
 *n = 98;
-
 }
