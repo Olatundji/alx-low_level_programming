4abf3760721e08d7ee9913b0a7f740cd7f6b42e9
Olatundji <oikpaye@gmail.com>
2022-06-13 06:46:54 -0700

    Programming in c to day

---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,23 @@
+nclude <unistd.h>
+
+/**
+ *
+ *  * _putchar - write the char c to standard output
+ *
+ *   * @: The character to print
+ *
+ *    * Return: on success 1.
+ *
+ *     * On error: -1 is returned , and error is there returned
+ *
+ *      */
+
+
+
+int _putchar(char c)
+
+{
+
+	    return (write(1, &c, 1));
+
+}
