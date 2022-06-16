6a400271e36d3420d33664d49af784d19b583261
Olatundji <104756391+Olatundji@users.noreply.github.com>
2022-06-16 20:46:53 +0100

    Update 5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,47 +1,24 @@
-nclude "main.h"
-
-
+#include "main.h"
 
 /**
+ *string_toupper - changes all lowercase letters of a string to uppercase.
+ *@s: string to modify.
  *
- *  *string_toupper - changes all lowercase letters of a string to uppercase.
- *
- *   *@s: string to modify.
- *
- *    *
- *
- *     *Return: s modified.
- *
- *      */
-
-
+ *Return: s modified.
+ */
 
 char *string_toupper(char *s)
-
 {
+int i = 0;
 
-	int i = 0;
+while (s[i] != '\0')
+{
+if (s[i] > 96 && s[i] < 123)
+{
+s[i] -= 32;
+}
+i++;
+}
 
-
-
-	while (s[i] != '\0')
-
-	{
-
-		if (s[i] > 96 && s[i] < 123)
-
-		{
-
-			s[i] -= 32;
-
-		}
-
-		i++;
-
-	}
-
-
-
-	return (s);
-
+return (s);
 }
