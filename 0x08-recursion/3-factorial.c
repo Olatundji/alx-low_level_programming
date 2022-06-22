13cc504f9d6bc0f6c6d3a2e0a4acf0869094a168
Olatundji <104756391+Olatundji@users.noreply.github.com>
2022-06-22 19:04:31 +0100

    Update 3-factorial.c

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,39 +1,20 @@
-nclude "main.h"
-
+#include "main.h"
 /**
- *
- *  *factorial - length of a string
- *
- *   *@n: pointer block of memory to fill
- *
- *    *Return: factorial
- *
- *     */
-
-
-
+ *factorial - length of a string
+ *@n: pointer block of memory to fill
+ *Return: factorial
+ */
 
 
 int factorial(int n)
-
 {
+if (n == 0)
 
-	if (n == 0)
+return (1);
 
-
-
-		return (1);
-
-
-
-	else if (n < 0)
-
-		return (-1);
-
-	else
-
-		return (n * factorial(n - 1));
-
-
+else if (n < 0)
+return (-1);
+else
+return (n * factorial(n - 1));
 
 }
