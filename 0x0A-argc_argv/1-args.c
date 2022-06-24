58025330facb897ef4266070073446a672281a06
Olatundji <104756391+Olatundji@users.noreply.github.com>
2022-06-24 18:09:38 +0100

    Update 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,41 +1,21 @@
-nclude <stdio.h>
-
+#include <stdio.h>
 #include <stdlib.h>
 
-
-
 /**
- *
- *  *main - prints number of arguments passed
- *
- *   *@argc: first parameter
- *
- *    *@argv: second parameter
- *
- *     *DESCRIPTION: prints number of arguments
- *
- *      *Return: Always (0) Success
- *
- *       */
-
-
+ *main - prints number of arguments passed
+ *@argc: first parameter
+ *@argv: second parameter
+ *DESCRIPTION: prints number of arguments
+ *Return: Always (0) Success
+ */
 
 int main(int argc, char **argv)
-
 {
-
-	int i;
-
-	for (i = 0; i < argc; i++)
-
-	{
-
-		argv[i] = argv[i];
-
-	}
-
-	printf("%d\n", i - 1);
-
-	return (0);
-
+int i;
+for (i = 0; i < argc; i++)
+{
+argv[i] = argv[i];
+}
+printf("%d\n", i - 1);
+return (0);
 }
