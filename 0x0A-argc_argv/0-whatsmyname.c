0fa2658b4f3a2c61edd40cc757e2398d68d2e099
Olatundji <104756391+Olatundji@users.noreply.github.com>
2022-06-24 18:41:40 +0100

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,31 +1,16 @@
-nclude <stdio.h>
-
+#include <stdio.h>
 #include <stdlib.h>
 
-
-
 /**
- *
- *  * main - Entry point
- *
- *   * @argc: count the number of parameters that goes into the main.
- *
- *    * @argv: pointer of array of pointers that contains strings entering main.
- *
- *     * Return: 1 if successful.
- *
- *      */
-
-
+ * main - Entry point
+ * @argc: count the number of parameters that goes into the main.
+ * @argv: pointer of array of pointers that contains strings entering main.
+ * Return: 1 if successful.
+ */
 
 int main(int argc, char **argv)
-
 {
-
-	if (argc > 0)
-
-		printf("%s\n", argv[0]);
-
-	return (0);
-
+if (argc > 0)
+printf("%s\n", argv[0]);
+return (0);
 }
