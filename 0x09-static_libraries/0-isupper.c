5591947e482f885011eeb4506dd37f5e0c921214
Olatundji <104756391+Olatundji@users.noreply.github.com>
2022-06-28 21:23:28 +0100

    Update 0-isupper.c

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,98 @@
+cd /root
+cd alx-low_level_programming
+mkdir 0x09-static_libraries
+cd 0x09-static_libraries
+echo 'progamming in C' > README.md
+cat README.md
+git add .
+git commit -m "c- programming"
+git push
+touch 0-isupper.c
+chmod u+x 0-isupper.c
+touch 0-isupper.o
+chmod u+x 0-isupper.o
+touch 0-memset.c
+chmod u+x 0-memset.c
+touch 0-strcat.c
+chmod u+x 0-strcat.c
+touch 0-strcat.o
+chmod u+x 0-strcat.o
+touch 1-isdigit.c
+chmod u+x 1-isdigit.c
+touch 1-isdigit.o
+chmod u+x 1-isdigit.o
+touch 1-memcpy.c
+chmod u+x 1-memcpy.c
+touch 1-memcpy.o
+chmod u+x 1-memcpy.o
+touch 1-strncat.c
+chmod u+x 1-strncat.c
+touch 1-strncat.o
+chmod u+x 1-strncat.o
+touch 2-strchr.c
+chmod u+x 2-strchr.c
+touch 2-strchr.o
+chmod u+x 2-strchr.o
+touch 2-strlen.c
+chmod u+x 2-strlen.c
+touch 2-strlen.o
+chmod u+x 2-strlen.o
+touch 2-strncpy.c
+chmod u+x 2-strncpy.c
+touch 2-strncpy.o
+chmod u+x 2-strncpy.o
+touch 3-islower.c
+chmod u+x 3-islower.c
+touch 3-islower.o
+chmod u+x 3-islower.o
+touch 3-puts.c
+chmod u+x 3-puts.c
+touch 3-puts.o
+chmod u+x 3-puts.o
+touch 3-strcmp.c
+chmod u+x 3-strcmp.c
+touch 3-strcmp.o
+chmod u+x 3-strcmp.o
+touch 3-strspn.c
+chmod u+x 3-strspn.c
+touch 3-strspn.o
+chmod u+x 3-strspn.o
+touch 4-isalpha.c
+chmod u+x 4-isalpha.c
+touch 4-isalpha.o
+chmod u+x 4-isalpha.o
+touch 4-strpbrk.c
+chmod u+x 4-strpbrk.c
+touch 4-strpbrk.o
+chmod u+x 4-strpbrk.o
+touch 5-strstr.c
+chmod u+x 5-strstr.c
+touch 5-strstr.o
+chmod u+x 5-strstr.o
+touch 6-abs.c
+chmod u+x 6-abs.c
+touch 6-abs.o
+chmod u+x 6-abs.o
+touch 9-strcpy.c
+chmod u+x 9-strcpy.c
+touch 9-strcpy.o
+chmod u+x 9-strcpy.o
+touch _putchar.c
+chmod u+x _putchar.c
+touch _putchar.o
+chmod u+x _putchar.o
+touch create_static_lib.sh
+chmod u+x create_static_lib.sh
+touch liball.a
+chmod u+x liball.a
+touch quote
+chmod u+x quote
+touch main.c
+chmod u+x main.c
+touch libmy.a
+chmod u+x libmy.a
+touch main.h
+chmod u+x main.h
+git add .
+git commit -m "Programming in c to day"
+git push
