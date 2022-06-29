d4bc16c203ebe1bbb5f75c0f886b0136ee236e61
Olatundji <104756391+Olatundji@users.noreply.github.com>
2022-06-29 23:05:38 +0100

    Update 1-isdigit.c

--- 0x09-static_libraries/1-isdigit.c
+++ 0x09-static_libraries/1-isdigit.c
@@ -1,21 +1,11 @@
-nclude "main.h"
-
+#include "main.h"
 /**
- *
- *  * _isdigit - evaluates if its a digit
- *
- *   * @c: var to be evaluated
- *
- *    * Return: 1 if a digit 0  otherwise
- *
- *     */
-
-
+ * _isdigit - evaluates if its a digit
+ * @c: var to be evaluated
+ * Return: 1 if a digit 0  otherwise
+ */
 
 int _isdigit(int c)
-
 {
-
-		return (c >= 48 && c <= 57);
-
+	return (c >= 48 && c <= 57);
 }
