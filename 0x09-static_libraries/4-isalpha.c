9b84c66dd8c8c4b2b731ae9e9bd9a814b1cd2808
Olatundji <104756391+Olatundji@users.noreply.github.com>
2022-06-29 23:13:30 +0100

    Update 4-isalpha.c

--- 0x09-static_libraries/4-isalpha.c
+++ 0x09-static_libraries/4-isalpha.c
@@ -1,43 +1,22 @@
-nclude "main.h"
-
-
+#include "main.h"
 
 /**
- *
- *  * _isalpha -checks if c is alphabetic character
- *
- *   * @c: the value to be checked
- *
- *    * Return: 1 if alphabetic character else 0
- *
- *     */
-
-
+ * _isalpha -checks if c is alphabetic character
+ * @c: the value to be checked
+ * Return: 1 if alphabetic character else 0
+ */
 
 int _isalpha(int c)
-
 {
 
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return (1);
 
+	}
+	else
+	{
+		return (0);
 
-		if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-
-				{
-
-							return (1);
-
-
-
-								}
-
-			else
-
-					{
-
-								return (0);
-
-
-
-									}
-
+	}
 }
