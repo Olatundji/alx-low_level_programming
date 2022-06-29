e8634d74a93eba841d3bf2b0088d4d20be554837
Olatundji <104756391+Olatundji@users.noreply.github.com>
2022-06-29 23:06:14 +0100

    Update 1-memcpy.c

--- 0x09-static_libraries/1-memcpy.c
+++ 0x09-static_libraries/1-memcpy.c
@@ -1,39 +1,20 @@
-nclude "main.h"
-
+#include "main.h"
 /**
- *
- *  * *_memcpy - a function that copies memory area.
- *
- *   * @dest: pointer to memory area to print to
- *
- *    * @src: pointer to memory area to copy from
- *
- *     * @n: number of bytes to copy
- *
- *      * Return: dest
- *
- *       */
-
+ * *_memcpy - a function that copies memory area.
+ * @dest: pointer to memory area to print to
+ * @src: pointer to memory area to copy from
+ * @n: number of bytes to copy
+ * Return: dest
+ */
 char *_memcpy(char *dest, char *src, unsigned int n)
-
 {
+	unsigned int i = 0;
 
-		unsigned int i = 0;
+	while (i < n)
+	{
 
-
-
-			while (i < n)
-
-					{
-
-
-
-							dest[i] = src[i];
-
-								i++;
-
-									}
-
-				return (dest);
-
+	dest[i] = src[i];
+	i++;
+	}
+	return (dest);
 }
