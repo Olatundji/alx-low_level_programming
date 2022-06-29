70030771c3f8edbcf62c903ed4490095e5bca2ce
Olatundji <104756391+Olatundji@users.noreply.github.com>
2022-06-29 23:09:31 +0100

    Update 2-strncpy.c

--- 0x09-static_libraries/2-strncpy.c
+++ 0x09-static_libraries/2-strncpy.c
@@ -1,67 +1,34 @@
-nclude "main.h"
-
+#include "main.h"
 /**
- *
- *  * *_strncpy - copies a string
- *
- *   * @dest: string pointer
- *
- *    * @src: string pointer
- *
- *     * @n: number of bytes to use from src
- *
- *      * Return: string
- *
- *       */
-
+ * *_strncpy - copies a string
+ * @dest: string pointer
+ * @src: string pointer
+ * @n: number of bytes to use from src
+ * Return: string
+ */
 char *_strncpy(char *dest, char *src, int n)
-
 {
 
+	int i = 0;
+	int j = 0;
 
+	while (n > j)
+	{
+		if (src[j] == '\0')
+		{
+			for (; j < n; j++)
+			{
+			dest[i] = '\0';
+			i++;
+			}
+		}
+		else
+		{
+		dest[i] = src[j];
+		j++;
+		i++;
+		}
+	}
 
-		int i = 0;
-
-			int j = 0;
-
-
-
-				while (n > j)
-
-						{
-
-									if (src[j] == '\0')
-
-												{
-
-																for (; j < n; j++)
-
-																				{
-
-																								dest[i] = '\0';
-
-																											i++;
-
-																														}
-
-																		}
-
-											else
-
-														{
-
-																	dest[i] = src[j];
-
-																			j++;
-
-																					i++;
-
-																							}
-
-												}
-
-
-
-					return (dest);
-
+	return (dest);
 }
