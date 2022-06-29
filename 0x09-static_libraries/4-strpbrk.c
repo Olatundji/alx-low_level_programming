7d1c711d6086055fdbb8c6e83dce3fec12a40a84
Olatundji <104756391+Olatundji@users.noreply.github.com>
2022-06-29 23:14:06 +0100

    Update 4-strpbrk.c

--- 0x09-static_libraries/4-strpbrk.c
+++ 0x09-static_libraries/4-strpbrk.c
@@ -1,57 +1,29 @@
-nclude "main.h"
-
+#include "main.h"
 /**
- *
- *  * *_strpbrk - searches a string for any of a set of bytes
- *
- *   * @s: string
- *
- *    * @accept: string to search
- *
- *     * Return: pointer to searched string
- *
- *      */
-
+ * *_strpbrk - searches a string for any of a set of bytes
+ * @s: string
+ * @accept: string to search
+ * Return: pointer to searched string
+ */
 char *_strpbrk(char *s, char *accept)
-
 {
+	unsigned int i, j;
 
-		unsigned int i, j;
+	for (i = 0; *(s + i); i++)
+	{
 
+	for (j = 0; *(accept + j); j++)
+	{
 
-
-			for (i = 0; *(s + i); i++)
-
-					{
-
-
-
-							for (j = 0; *(accept + j); j++)
-
-									{
-
-
-
-											if (*(s + i) == *(accept + j))
-
-													{
-
-															break;
-
-																}
-
-												}
-
-								if (*(accept + j) != '\0')
-
-										{
-
-												return (s + i);
-
-													}
-
-									}
-
-				return (0);
-
+	if (*(s + i) == *(accept + j))
+	{
+	break;
+	}
+	}
+	if (*(accept + j) != '\0')
+	{
+	return (s + i);
+	}
+	}
+	return (0);
 }
