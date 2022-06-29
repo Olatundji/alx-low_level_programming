e8e24b10997e094da290f41f9790c2a6c6f6b888
Olatundji <104756391+Olatundji@users.noreply.github.com>
2022-06-29 23:15:12 +0100

    Update 6-abs.c

--- 0x09-static_libraries/6-abs.c
+++ 0x09-static_libraries/6-abs.c
@@ -1,67 +1,34 @@
-nclude "main.h"
-
+#include "main.h"
 /**
- *
- *  * _abs - computes absolute value of an integer
- *
- *   * @n: takes in the integer
- *
- *    * Return: always 0 (success)
- *
- *     */
-
+ * _abs - computes absolute value of an integer
+ * @n: takes in the integer
+ * Return: always 0 (success)
+ */
 int _abs(int n)
-
-
 
 {
 
+	if (n < 0)
 
+	{
 
-		if (n < 0)
+		return (n * (-1));
 
+	}
 
+	else if (n == 0)
 
-				{
+	{
 
+		return (0);
 
+	}
 
-							return (n * (-1));
+	else
 
+	{
 
+		return (n);
 
-								}
-
-
-
-			else if (n == 0)
-
-
-
-					{
-
-
-
-								return (0);
-
-
-
-									}
-
-
-
-				else
-
-
-
-						{
-
-
-
-									return (n);
-
-
-
-										}
-
+	}
 }
