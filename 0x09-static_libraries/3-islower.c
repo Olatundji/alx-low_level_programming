9156c298ab01654edd0d835987f4cb5e965e5cd2
Olatundji <104756391+Olatundji@users.noreply.github.com>
2022-06-29 23:10:17 +0100

    Update 3-islower.c

--- 0x09-static_libraries/3-islower.c
+++ 0x09-static_libraries/3-islower.c
@@ -1,33 +1,17 @@
-nclude "main.h"
-
-
+#include "main.h"
 
 /**
- *
- *  * _islower - checks if a character is lowercase
- *
- *   *@c: The chacter to be checked
- *
- *    * Return: 1 if a character is lowercase 0 otherwise
- *
- *     */
-
+ * _islower - checks if a character is lowercase
+ *@c: The chacter to be checked
+ * Return: 1 if a character is lowercase 0 otherwise
+ */
 int _islower(int c)
-
 {
 
+	if (c >= 'a' && c <= 'z')
 
-
-		if (c >= 'a' && c <= 'z')
-
-
-
-				return (1);
-
-			else
-
-					return (0);
-
-
+	return (1);
+	else
+	return (0);
 
 }
