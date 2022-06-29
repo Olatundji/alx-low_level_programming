f3809ff8ff0269d812e745ea836745258c8ee51e
Olatundji <oikpaye@gmail.com>
2022-06-29 15:04:35 -0700

    Programming in c to day

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,33 @@
+nclude "main.h"
+
+/**
+ *
+ *  * _puts - prints string
+ *
+ *   * @str: pointer character
+ *
+ *    * Return void
+ *
+ *     */
+
+void _puts(char *str)
+
+{
+
+
+
+		int i;
+
+
+
+			for (i = 0; str[i] != '\0'; i++)
+
+					{
+
+							_putchar(str[i]);
+
+								}
+
+				_putchar('\n');
+
+}
---
+++ 0x09-static_libraries/main.c
@@ -0,0 +1,21 @@
+nclude "main.h"
+
+
+
+/**
+ *
+ *  * main function
+ *
+ *   */
+
+
+
+int main(void)
+
+{
+
+	_puts("\"At the end of the day, my goal was to be the best hacker\"\n");
+
+	return (0);
+
+}
