8b91a9242010c4c84a0c017a2bc87b12d48ef137
Olatundji <104756391+Olatundji@users.noreply.github.com>
2022-06-30 09:47:57 +0100

    Update 2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,61 +1,31 @@
-nclude "main.h"
-
+#include "main.h"
 #include <stdio.h>
-
 #include <stdlib.h>
 
-
-
 /**
+ * _calloc - function that allocates memory for an array
+ * @nmemb: first parameter
+ * @size: second parameter
  *
- *  * _calloc - function that allocates memory for an array
- *
- *   * @nmemb: first parameter
- *
- *    * @size: second parameter
- *
- *     *
- *
- *      * Description: function that allocates memory for an array
- *
- *       * Return: Always(0) Success
- *
- *        */
-
+ * Description: function that allocates memory for an array
+ * Return: Always(0) Success
+ */
 void *_calloc(unsigned int nmemb, unsigned int size)
-
 {
+char *memory;
+unsigned int b;
 
-	char *memory;
+if (nmemb == 0 || size == 0)
+return (NULL);
 
-	unsigned int b;
+memory = malloc(nmemb * size);
 
+if (memory == NULL)
+return (NULL);
 
-
-	if (nmemb == 0 || size == 0)
-
-		return (NULL);
-
-
-
-	memory = malloc(nmemb * size);
-
-
-
-	if (memory == NULL)
-
-		return (NULL);
-
-
-
-	for (b = 0; b < (nmemb * size); b++)
-
-	{
-
-		*(memory + b) = 0;
-
-	}
-
-	return (memory);
-
+for (b = 0; b < (nmemb * size); b++)
+{
+*(memory + b) = 0;
+}
+return (memory);
 }
