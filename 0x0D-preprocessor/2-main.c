fd088ca9d062fee15903a94eb23013d2b6c7ef1b
Olatundji <104756391+Olatundji@users.noreply.github.com>
2022-07-05 10:21:17 +0100

    Update 2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,21 +1,11 @@
-nclude <stdlib.h>
-
+#include <stdlib.h>
 #include <stdio.h>
-
 /**
- *
- *   *main - prints name of file.
- *
- *     *Return: 0.
- *
- *       */
-
+  *main - prints name of file.
+  *Return: 0.
+  */
 int main(void)
-
 {
-
-		printf("%s\n", __FILE__);
-
-			return (0);
-
+	printf("%s\n", __FILE__);
+	return (0);
 }
