928cbe30a540d369796f57bf8e99b2113ae80444
Olatundji <104756391+Olatundji@users.noreply.github.com>
2022-07-14 23:40:12 +0100

    Update 1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,39 +1,20 @@
-nclude "lists.h"
-
-
+#include "lists.h"
 
 /**
- *
- *  *list_len - returns the number of elements
- *
- *   *in a linked list list_t
- *
- *    *@h: pointer to the first node
- *
- *     *Return: number of lements
- *
- *      */
-
-
+ *list_len - returns the number of elements
+ *in a linked list list_t
+ *@h: pointer to the first node
+ *Return: number of lements
+ */
 
 size_t list_len(const list_t *h)
-
 {
+size_t count = 0;
 
-	size_t count = 0;
-
-
-
-	while (h)
-
-	{
-
-		h = h->next;
-
-		count++;
-
-	}
-
-	return (count);
-
+while (h)
+{
+h = h->next;
+count++;
+}
+return (count);
 }
