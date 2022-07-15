90deab8f2ebc8557bccffa46a371c42667d8bc43
Olatundji <104756391+Olatundji@users.noreply.github.com>
2022-07-15 14:55:41 +0100

    Update strlen.c

--- 0x12-singly_linked_lists/strlen.c
+++ 0x12-singly_linked_lists/strlen.c
@@ -1,37 +1,19 @@
-nclude "lists.h"
-
-
+#include "lists.h"
 
 /**
+ * _strlen - return length of string
  *
- *  * _strlen - return length of string
- *
- *   *
- *
- *    * @str: string
- *
- *     * Return: length of string
- *
- *      */
-
+ * @str: string
+ * Return: length of string
+ */
 int _strlen(char *str)
-
 {
-
-		if (*str == '\0')
-
-				{
-
-							return (0);
-
-								}
-
-			else
-
-					{
-
-								return (1 + _strlen(str + 1));
-
-									}
-
+	if (*str == '\0')
+	{
+		return (0);
+	}
+	else
+	{
+		return (1 + _strlen(str + 1));
+	}
 }
