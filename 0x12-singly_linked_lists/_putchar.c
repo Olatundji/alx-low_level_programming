b802c6a0479f7ec35337a1386e9b18cdb32180a1
Olatundji <104756391+Olatundji@users.noreply.github.com>
2022-07-15 14:57:20 +0100

    Update _putchar.c

--- 0x12-singly_linked_lists/_putchar.c
+++ 0x12-singly_linked_lists/_putchar.c
@@ -1,27 +1,14 @@
-nclude <unistd.h>
-
-
+#include <unistd.h>
 
 /**
+ *  _putchar - Takes in char c and outputs to stdout
+ *  @c: Character c to output to stdout
  *
- *  *  _putchar - Takes in char c and outputs to stdout
+ *  Return: Always 0 (Success)
  *
- *   *  @c: Character c to output to stdout
- *
- *    *
- *
- *     *  Return: Always 0 (Success)
- *
- *      *
- *
- *       */
-
-
+ */
 
 int _putchar(char c)
-
 {
-
-		return (write(1, &c, 1));
-
+	return (write(1, &c, 1));
 }
