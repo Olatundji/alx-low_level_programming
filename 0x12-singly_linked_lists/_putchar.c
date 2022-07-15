c2ecf06e306414539f74837d662b561d07294874
Olatundji <oikpaye@gmail.com>
2022-07-15 06:54:27 -0700

    c- programming

---
+++ 0x12-singly_linked_lists/_putchar.c
@@ -0,0 +1,27 @@
+nclude <unistd.h>
+
+
+
+/**
+ *
+ *  *  _putchar - Takes in char c and outputs to stdout
+ *
+ *   *  @c: Character c to output to stdout
+ *
+ *    *
+ *
+ *     *  Return: Always 0 (Success)
+ *
+ *      *
+ *
+ *       */
+
+
+
+int _putchar(char c)
+
+{
+
+		return (write(1, &c, 1));
+
+}
---
+++ 0x12-singly_linked_lists/_strlen.c
@@ -0,0 +1,37 @@
+nclude "lists.h"
+
+
+
+/**
+ *
+ *  * _strlen - return length of string
+ *
+ *   *
+ *
+ *    * @str: string
+ *
+ *     * Return: length of string
+ *
+ *      */
+
+int _strlen(char *str)
+
+{
+
+		if (*str == '\0')
+
+				{
+
+							return (0);
+
+								}
+
+			else
+
+					{
+
+								return (1 + _strlen(str + 1));
+
+									}
+
+}
