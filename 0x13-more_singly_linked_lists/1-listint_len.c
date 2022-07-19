5928e949b97a3100c31d2dd534508962c3385959
Olatundji <104756391+Olatundji@users.noreply.github.com>
2022-07-19 17:01:30 +0100

    Update 1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,35 +1,18 @@
-nclude "lists.h"
-
+#include "lists.h"
 /**
- *
- *  *listint_len - returns the number of elements in
- *
- *   *a linked list
- *
- *    *@h: pointer to the first element
- *
- *     *Return: the number of elements
- *
- *      */
-
+ *listint_len - returns the number of elements in
+ *a linked list
+ *@h: pointer to the first element
+ *Return: the number of elements
+ */
 size_t listint_len(const listint_t *h)
-
 {
+size_t count = 0;
 
-	size_t count = 0;
-
-
-
-	while (h)
-
-	{
-
-		h = h->next;
-
-		count++;
-
-	}
-
-	return (count);
-
+while (h)
+{
+h = h->next;
+count++;
+}
+return (count);
 }
