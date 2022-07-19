8150459cefa121d87ce864479d93613bc55da0ee
Olatundji <104756391+Olatundji@users.noreply.github.com>
2022-07-19 17:08:54 +0100

    Update 10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,35 +1,47 @@
 #include "lists.h"
-/**
- * delete_nodeint_at_index -deletes a node
- * @head: pointer to the first node
- * @idx: index to the first node
- * Return: 1 if success, - 1 if fails
- */
-int delete_nodeint_at_index(listint_t **head, unsigned int idx)
-{
-listint_t *temp, *temp_addr;
-unsigned int count;
 
-temp = *head;
-count = 0;
-while (temp)
+/**
+ * delete_nodeint_at_index - deletes the node at index
+ * of a linked list.
+ * @head: head of a list.
+ * @index: index of the list where the node is
+ * deleted.
+ *
+ * Return: 1 if it succeeded, -1 if it failed.
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-if (count == (idx - 1))
-{
-temp_addr = (temp->next)->next;
-free(temp->next);
-temp->next = temp_addr;
-return (1);
-}
-else if (idx == 0)
-{
-temp = *head;
-*head = (*head)->next;
-free(temp);
-return (1);
-}
-count++;
-temp = temp->next;
-}
-return (-1);
+	unsigned int i;
+	listint_t *prev;
+	listint_t *next;
+
+	prev = *head;
+
+	if (index != 0)
+	{
+		for (i = 0; i < index - 1 && prev != NULL; i++)
+		{
+			prev = prev->next;
+		}
+	}
+
+	if (prev == NULL || (prev->next == NULL && index != 0))
+	{
+		return (-1);
+	}
+
+	next = prev->next;
+
+	if (index != 0)
+	{
+		prev->next = next->next;
+		free(next);
+	}
+	else
+	{
+		free(prev);
+		*head = next;
+	}
+
+	return (1);
 }
