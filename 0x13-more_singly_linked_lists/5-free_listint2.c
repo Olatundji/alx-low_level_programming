dd4c7fc6637e48e6ae310c8e40a96c4a10099aff
Olatundji <104756391+Olatundji@users.noreply.github.com>
2022-07-19 17:16:11 +0100

    Update 5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,39 +1,20 @@
-nclude "lists.h"
-
+#include "lists.h"
 /**
- *
- *  * free_listint2 - frees a list and sets the heaed to Null
- *
- *   * @head:  pointer to the begining of the string
- *
- *    */
-
+ * free_listint2 - frees a list and sets the heaed to Null
+ * @head:  pointer to the begining of the string
+ */
 void free_listint2(listint_t **head)
-
 {
+listint_t *current, *next;
 
-	listint_t *current, *next;
-
-
-
-	if (head == NULL)
-
-		return;
-
-	current = *head;
-
-	while (current)
-
-	{
-
-		next = current->next;
-
-		free(current);
-
-		current = next;
-
-	}
-
-	*head = NULL;
-
+if (head == NULL)
+return;
+current = *head;
+while (current)
+{
+next = current->next;
+free(current);
+current = next;
+}
+*head = NULL;
 }
