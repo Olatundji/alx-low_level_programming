4e987e055e64156ee6c72104d3d4d0fe8a731137
Olatundji <104756391+Olatundji@users.noreply.github.com>
2022-09-02 00:24:06 +0100

    Update 6-sum_dlistint.c

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,39 +1,20 @@
-nclure  " listes.h "
-
-
+inclure  " listes.h "
 
 /* *
- *
- * * sum_dlistint - Additionne toutes les données d'une liste dlistint_t.
- *
- * * @head : La tête de la liste dlistint_t.
- *
- * *
- *
- * * Retour : La somme de toutes les données.
- *
- *  */
-
+* sum_dlistint - Additionne toutes les données d'une liste dlistint_t.
+* @head : La tête de la liste dlistint_t.
+*
+* Retour : La somme de toutes les données.
+ */
 int  sum_dlistint ( dlistint_t *head)
-
 {
+	entier somme = 0 ;
 
-		entier somme = 0 ;
+	tandis que (tête)
+	{
+		somme += tête-> n ;
+		tête = tête-> suivant ;
+	}
 
-
-
-			tandis que (tête)
-
-					{
-
-								somme += tête-> n ;
-
-										tête = tête-> suivant ;
-
-											}
-
-
-
-				retour (somme);
-
+	retour (somme);
 }
