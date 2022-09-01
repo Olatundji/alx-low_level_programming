ebc9f63fb936acdd49fc0ccdb9059282e8ec04b5
Olatundji <oikpaye@gmail.com>
2022-09-01 16:09:48 -0700

    programming

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,39 @@
+nclure  " listes.h "
+
+
+
+/* *
+ *
+ * * dlistint_len - Compte le nombre d'éléments dans une liste liée dlistint_t.
+ *
+ * * @h : La tête de la liste dlistint_t.
+ *
+ * *
+ *
+ * * Retour : Le nombre d'éléments dans la liste dlistint_t.
+ *
+ *  */
+
+size_t  dlistint_len ( const  dlistint_t *h)
+
+{
+
+		taille_t nœuds = 0 ;
+
+
+
+			tandis que (h)
+
+					{
+
+								nœuds++ ;
+
+										h = h-> suivant ;
+
+											}
+
+
+
+				retour (nœuds);
+
+}
