ab6ab35fd2c47b7842449843dc239a69eaa5fb6b
Olatundji <104756391+Olatundji@users.noreply.github.com>
2022-09-02 00:13:11 +0100

    Update 1-dlistint_len.c

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,39 +1,20 @@
-nclure  " listes.h "
-
-
+# inclure  " listes.h "
 
 /* *
- *
- * * dlistint_len - Compte le nombre d'éléments dans une liste liée dlistint_t.
- *
- * * @h : La tête de la liste dlistint_t.
- *
- * *
- *
- * * Retour : Le nombre d'éléments dans la liste dlistint_t.
- *
- *  */
-
+* dlistint_len - Compte le nombre d'éléments dans une liste liée dlistint_t.
+* @h : La tête de la liste dlistint_t.
+*
+* Retour : Le nombre d'éléments dans la liste dlistint_t.
+ */
 size_t  dlistint_len ( const  dlistint_t *h)
-
 {
+	taille_t nœuds = 0 ;
 
-		taille_t nœuds = 0 ;
+	tandis que (h)
+	{
+		nœuds++ ;
+		h = h-> suivant ;
+	}
 
-
-
-			tandis que (h)
-
-					{
-
-								nœuds++ ;
-
-										h = h-> suivant ;
-
-											}
-
-
-
-				retour (nœuds);
-
+	retour (nœuds);
 }
