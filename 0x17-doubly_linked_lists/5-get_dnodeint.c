94b9c407384778802b730920d55b67c1949cb710
Olatundji <104756391+Olatundji@users.noreply.github.com>
2022-09-02 00:23:00 +0100

    Update 5-get_dnodeint.c

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,41 +1,21 @@
-nclure  " listes.h "
-
-
+# inclure  " listes.h "
 
 /* *
- *
- * * get_dnodeint_at_index - Localise un nœud dans une liste dlistint_t.
- *
- * * @head : La tête de la liste dlistint_t.
- *
- * * @index : le nœud à localiser.
- *
- * *
- *
- * * Retour : Si le nœud n'existe pas - NULL.
- *
- * * Sinon - l'adresse du nœud localisé.
- *
- *  */
-
+* get_dnodeint_at_index - Localise un nœud dans une liste dlistint_t.
+* @head : La tête de la liste dlistint_t.
+* @index : le nœud à localiser.
+*
+* Retour : Si le nœud n'existe pas - NULL.
+* Sinon - l'adresse du nœud localisé.
+ */
 dlistint_t * get_dnodeint_at_index ( dlistint_t *head, index int non signé ) 
-
 {
+	pour (; indice != 0 ; indice --)
+	{
+		si (tête == NULL )
+			retour ( NULL );
+		tête = tête-> suivant ;
+	}
 
-		pour (; indice != 0 ; indice --)
-
-				{
-
-							si (tête == NULL )
-
-											retour ( NULL );
-
-									tête = tête-> suivant ;
-
-										}
-
-
-
-			retour (tête);
-
+	retour (tête);
 }
