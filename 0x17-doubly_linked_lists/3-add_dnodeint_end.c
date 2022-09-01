ea92d0767c8e8a738a7126004c7bdb9dacdfc209
Olatundji <104756391+Olatundji@users.noreply.github.com>
2022-09-02 00:20:35 +0100

    Update 3-add_dnodeint_end.c

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,71 +1,36 @@
-nclure  " listes.h "
-
-
+# inclure  " listes.h "
 
 /* *
- *
- * * add_dnodeint_end - Ajoute un nouveau nœud à la fin d'une liste dlistint_t.
- *
- * * @head : un pointeur vers l'en-tête de la liste dlistint_t.
- *
- * * @n : L'entier que le nouveau nœud doit contenir.
- *
- * *
- *
- * * Retour : Si la fonction échoue - NULL.
- *
- * * Sinon - l'adresse du nouveau nœud.
- *
- *  */
-
+* add_dnodeint_end - Ajoute un nouveau nœud à la fin d'une liste dlistint_t.
+* @head : un pointeur vers l'en-tête de la liste dlistint_t.
+* @n : L'entier que le nouveau nœud doit contenir.
+*
+* Retour : Si la fonction échoue - NULL.
+* Sinon - l'adresse du nouveau nœud.
+ */
 dlistint_t * add_dnodeint_end ( dlistint_t **head, const  int n)
-
 {
+	dlistint_t *nouveau, *dernier ;
 
-		dlistint_t *nouveau, *dernier ;
+	new = malloc ( sizeof ( dlistint_t ));
+	si (nouveau == NULL )
+		retour ( NULL );
 
+	nouveau-> n = n ;
+	nouveau-> suivant = NULL ;
 
+	si (*tête == NULL )
+	{
+		nouveau-> précédent = NULL ;
+		*tête = nouveau ;
+		retour (nouveau);
+	}
 
-			new = malloc ( sizeof ( dlistint_t ));
+	dernier = *tête ;
+	while (dernier-> suivant != NULL )
+		dernier = dernier-> suivant ;
+	dernier-> suivant = nouveau ;
+	nouveau-> précédent = dernier ;
 
-				si (nouveau == NULL )
-
-							retour ( NULL );
-
-
-
-					nouveau-> n = n ;
-
-						nouveau-> suivant = NULL ;
-
-
-
-							si (*tête == NULL )
-
-									{
-
-												nouveau-> précédent = NULL ;
-
-														*tête = nouveau ;
-
-																retour (nouveau);
-
-																	}
-
-
-
-								dernier = *tête ;
-
-									while (dernier-> suivant != NULL )
-
-												dernier = dernier-> suivant ;
-
-										dernier-> suivant = nouveau ;
-
-											nouveau-> précédent = dernier ;
-
-
-
-												retour (nouveau);
-
+	retour (nouveau);
 }
