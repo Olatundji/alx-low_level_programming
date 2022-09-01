b2f0e3cb03cd5e43cebd44cf4d2fe904d03a8de0
Olatundji <104756391+Olatundji@users.noreply.github.com>
2022-09-02 00:31:04 +0100

    Update 0-print_dlistint.c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,1 +1,26 @@
-ier : 0-print_dli
+/*
+* Fichier : 0-print_dlistint.c
+* Auteur : Ukonu Divine Chisom
+ */
+
+# inclure  " listes.h "
+
+/* *
+* print_dlistint - Imprime tous les éléments d'une liste dlistint_t.
+* @h : La tête de la liste dlistint_t.
+*
+* Retour : Le nombre de nœuds dans la liste.
+ */
+size_t  print_dlistint ( const  dlistint_t *h)
+{
+	taille_t nœuds = 0 ;
+
+	tandis que (h)
+	{
+		nœuds++ ;
+		printf ( " %d \n " , h-> n );
+		h = h-> suivant ;
+	}
+
+	retour (nœuds);
+}
