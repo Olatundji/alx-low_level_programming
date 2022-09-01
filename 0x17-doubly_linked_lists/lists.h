49783c52ad7cd3f0dbfbf4944d5b58dd5c125908
Olatundji <104756391+Olatundji@users.noreply.github.com>
2022-09-02 00:55:49 +0100

    Update lists.h

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,38 +1,38 @@
-# ifndef LISTS_H
-# définir  LISTS_H
+#ifndef LISTS_H
+#define LISTS_H
 
 /*
-* Fichier : listes.h
-* Auteur : Onyiana
+ * File: lists.h
+ * Author: Onyiana
  */
 
-# inclure  < stdio.h >
-# inclure  < stdlib.h >
+#include <stdio.h>
+#include <stdlib.h>
 
-/* *
-* struct dlistint_s - liste doublement liée
-* @n : entier
-* @prev : pointe vers le nœud précédent
-* @next : pointe vers le nœud suivant
-*
-* Description : structure de nœuds de liste à double lien
-* pour le projet Holberton
+/**
+ * struct dlistint_s - doubly linked list
+ * @n: integer
+ * @prev: points to the previous node
+ * @next: points to the next node
+ *
+ * Description: doubly linked list node structure
+ * for Holberton project
  */
-typedef  structure dlistint_s
+typedef struct dlistint_s
 {
-	int n ;
+	int n;
 	struct dlistint_s *prev;
-	struct dlistint_s *suivant ;
-} dlistint_t ;
+	struct dlistint_s *next;
+} dlistint_t;
 
-size_t  print_dlistint ( const  dlistint_t *h);
-size_t  dlistint_len ( const  dlistint_t *h);
-dlistint_t * add_dnodeint ( dlistint_t **head, const  int n);
-dlistint_t * add_dnodeint_end ( dlistint_t **head, const  int n);
-void  free_dlistint ( dlistint_t *head);
-dlistint_t * get_dnodeint_at_index ( dlistint_t *head, index int non signé  );
-int  sum_dlistint ( dlistint_t *head);
-dlistint_t * insert_dnodeint_at_index ( dlistint_t **h, int non signé  idx, int n);
-int  delete_dnodeint_at_index ( dlistint_t **tête, index int non signé  );
+size_t print_dlistint(const dlistint_t *h);
+size_t dlistint_len(const dlistint_t *h);
+dlistint_t *add_dnodeint(dlistint_t **head, const int n);
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
+void free_dlistint(dlistint_t *head);
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
+int sum_dlistint(dlistint_t *head);
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
 
-# endif  /* LISTS_H */
+#endif /* LISTS_H */
