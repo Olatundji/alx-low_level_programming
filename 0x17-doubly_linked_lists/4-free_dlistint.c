02ec437fdcfee8da86efb58e76c7ee89821c9a60
Olatundji <104756391+Olatundji@users.noreply.github.com>
2022-09-02 00:21:48 +0100

    Update 4-free_dlistint.c

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,33 +1,17 @@
-nclure  " listes.h "
-
-
+# inclure  " listes.h "
 
 /* *
- *
- * * free_dlistint - Libère une liste liée dlistint_t.
- *
- * * @head : La tête de la liste dlistint_t.
- *
- *  */
-
+* free_dlistint - Libère une liste liée dlistint_t.
+* @head : La tête de la liste dlistint_t.
+ */
 void  free_dlistint ( dlistint_t *head)
-
 {
+	dlistint_t *tmp;
 
-		dlistint_t *tmp;
-
-
-
-			tandis que (tête)
-
-					{
-
-								tmp = tête-> suivant ;
-
-										libre (tête);
-
-												tête = tmp ;
-
-													}
-
+	tandis que (tête)
+	{
+		tmp = tête-> suivant ;
+		libre (tête);
+		tête = tmp ;
+	}
 }
